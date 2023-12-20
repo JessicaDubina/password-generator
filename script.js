6bfe6acdfc6a0e680779aa039b3fc918512de318
jdub <jessicadubina@Austins-MBP-3.local>
2023-12-20 11:35:21 -0800

    added input form and stored input into js object

--- script.js
+++ script.js
@@ -13,13 +13,28 @@ function writePassword() {
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
 
+//add event listener for form submit and create function to add to new object
+document.getElementById("pwForm").addEventListener('submit', function(event) {
+  event.preventDefault();
 
+  //get form values
+  const pwlength = document.getElementById('pwlength').value;
+  const isLowercase = document.getElementsByName('isLowercaseinput')[0].checked;
+  const isUppercase = document.getElementsByName('isUppercaseinput')[0].checked;
+  const isNumeric = document.getElementsByName('isNumericinput')[0].checked;
+  const isSpecial = document.getElementsByName('isSpecialinput')[0].checked;
 
-//initialize object to store input from form
-let passwordInputs = {
-  pwlength: undefined, //min 8 char, max 128 char
-  isLowercase: true,
-  isUppercase: true,
-  isNumeric: true,
-  isSpecial: true
-}
+  //store form values in new object
+  const passwordInputs = {
+    pwlength: pwlength,
+    isLowercase: isLowercase,
+    isUppercase: isUppercase,
+    isNumeric: isNumeric,
+    isSpecial: isSpecial
+  }
+
+  //check that form data is added
+  console.log(passwordInputs);
+
+});
+
